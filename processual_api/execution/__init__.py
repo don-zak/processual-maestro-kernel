9a16b001ef592b292ab3abe73dc60f04b3973939
zaxam-tech <zaksam2030@gmail.com>
2026-08-08 17:50:16 +0100

    refactor(execution): export durable worker contracts

--- processual_api/execution/__init__.py
+++ processual_api/execution/__init__.py
@@ -4,20 +4,27 @@ from .durable import (
     DurableJobStore,
     ExecutionJob,
     ExecutionPriority,
+    IdempotencyConflictError,
     InMemoryDurableJobStore,
     JobLeaseLostError,
+    JobNotFoundError,
     JobSpec,
     JobStatus,
     RetryPolicy,
     SubmitResult,
 )
+from .worker import DurableWorker, JobHandler
 
 __all__ = [
     "DurableJobStore",
+    "DurableWorker",
     "ExecutionJob",
     "ExecutionPriority",
+    "IdempotencyConflictError",
     "InMemoryDurableJobStore",
+    "JobHandler",
     "JobLeaseLostError",
+    "JobNotFoundError",
     "JobSpec",
     "JobStatus",
     "RetryPolicy",
