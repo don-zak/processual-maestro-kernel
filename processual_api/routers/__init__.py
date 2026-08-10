98cd9df3d5090060170649ef6bef559aaf63b91a
zaxam-tech <zaksam2030@gmail.com>
2026-08-10 19:13:47 +0100

    feat: register sandbox failure review routes

--- processual_api/routers/__init__.py
+++ processual_api/routers/__init__.py
@@ -9,6 +9,9 @@ from . import (  # noqa: F401,E402
     settings_enterprise_endpoint_bindings_runtime as _settings_enterprise_endpoint_bindings_runtime,
 )
 from . import (  # noqa: F401,E402
+    settings_enterprise_endpoint_failure_review_runtime as _settings_enterprise_endpoint_failure_review_runtime,
+)
+from . import (  # noqa: F401,E402
     settings_enterprise_integration_runtime as _settings_enterprise_integration_runtime,
 )
 from . import settings_provider_test_runtime as _settings_provider_test_runtime  # noqa: F401,E402
