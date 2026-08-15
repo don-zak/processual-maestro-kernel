39377e82085bc9ceea860f92124dd7527e828959
zaxam-tech <zaksam2030@gmail.com>
2026-08-15 06:32:27 +0100

    style(admin): normalize security middleware import order

--- processual_api/middleware/security_headers.py
+++ processual_api/middleware/security_headers.py
@@ -1,6 +1,6 @@
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
 from starlette.responses import Response
+from starlette.requests import Request
 
 
 _ADMIN_DOM_CONTRACT_SCRIPT = (
