b330de374f91a67969ee2bc7e50559d74e206889
zaxam-tech <zaksam2030@gmail.com>
2026-08-18 12:01:43 +0100

    test: advance SQLite migration head to 0050

--- tests/test_sqlite_migration_chain.py
+++ tests/test_sqlite_migration_chain.py
@@ -7,8 +7,8 @@ import sys
 from pathlib import Path
 
 
-HEAD_REVISION = "20260818_0049"
-PREVIOUS_REVISION = "20260817_0048"
+HEAD_REVISION = "20260818_0050"
+PREVIOUS_REVISION = "20260818_0049"
 PARTIAL_DEFAULT_INDEX = "uq_admin_market_payment_destinations_active_default"
 
 
