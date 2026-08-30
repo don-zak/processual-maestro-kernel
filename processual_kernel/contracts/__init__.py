b3d14556e8ea0053dd0ce2f06df8995bd47795ab
zaxam-tech <zaksam2030@gmail.com>
2026-08-30 17:08:10 +0100

    refactor: export boundary contracts

--- processual_kernel/contracts/__init__.py
+++ processual_kernel/contracts/__init__.py
@@ -1,14 +1,21 @@
 """Stable runtime-agnostic contracts for Processual Maestro Kernel."""
 
+from .boundary import AgentRuntime, AgentSpec, AuditSink, MaestroEvent, WorkflowPlan, WorkflowStep
 from .enums import AgentCriticality, AgentState, MaestroAction, StepState, WorkflowState
 from .tasks import TaskEnvelope, TaskResult
 
 __all__ = [
     "AgentCriticality",
+    "AgentRuntime",
+    "AgentSpec",
     "AgentState",
+    "AuditSink",
     "MaestroAction",
+    "MaestroEvent",
     "StepState",
     "TaskEnvelope",
     "TaskResult",
+    "WorkflowPlan",
     "WorkflowState",
+    "WorkflowStep",
 ]
