90cf87047ec4e638a1a89e577a390ff451f2d3b8
zaxam-tech <zaksam2030@gmail.com>
2026-08-30 16:42:19 +0100

    refactor: export task transport contracts

--- processual_kernel/contracts/__init__.py
+++ processual_kernel/contracts/__init__.py
@@ -1,11 +1,14 @@
 """Stable runtime-agnostic contracts for Processual Maestro Kernel."""
 
 from .enums import AgentCriticality, AgentState, MaestroAction, StepState, WorkflowState
+from .tasks import TaskEnvelope, TaskResult
 
 __all__ = [
     "AgentCriticality",
     "AgentState",
     "MaestroAction",
     "StepState",
+    "TaskEnvelope",
+    "TaskResult",
     "WorkflowState",
 ]
