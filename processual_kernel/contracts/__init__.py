c15006c7d351b0946f24b9247862d7807cfb6f41
zaxam-tech <zaksam2030@gmail.com>
2026-08-30 16:10:06 +0100

    refactor: expose enum contract namespace

---
+++ processual_kernel/contracts/__init__.py
@@ -0,0 +1,11 @@
+"""Stable runtime-agnostic contracts for Processual Maestro Kernel."""
+
+from .enums import AgentCriticality, AgentState, MaestroAction, StepState, WorkflowState
+
+__all__ = [
+    "AgentCriticality",
+    "AgentState",
+    "MaestroAction",
+    "StepState",
+    "WorkflowState",
+]
