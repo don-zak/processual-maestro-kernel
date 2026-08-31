149f8afaa61c03a7f8df5fe7dce26d475f5f0727
zaxam-tech <zaksam2030@gmail.com>
2026-08-31 10:09:20 +0100

    Export evaluation runtime router explicitly

--- processual_api/routers/__init__.py
+++ processual_api/routers/__init__.py
@@ -5,7 +5,6 @@
 from . import cgt_governor_external_guard as _cgt_governor_external_guard  # noqa: F401,E402
 from . import client_api_keys_18 as _client_api_keys_18  # noqa: F401,E402
 from . import client_provider_alias_18 as _client_provider_alias_18  # noqa: F401,E402
-from . import evaluation_runtime as _evaluation_runtime  # noqa: F401,E402
 from . import institution_cases_18 as _institution_cases_18  # noqa: F401,E402
 from . import settings_admin_api_key_provisioning as _settings_admin_api_key_provisioning  # noqa: F401,E402
 from . import settings_admin_evaluation_grants as _settings_admin_evaluation_grants  # noqa: F401,E402
@@ -27,6 +26,7 @@ from .applications import router as applications_router
 from .cgt import router as cgt_router
 from .cgt_governor import router as cgt_governor_router
 from .discord import router as discord_router
+from .evaluation_runtime import router as evaluation_runtime_router
 from .execution_observability import router as execution_observability_router
 from .governance import router as governance_router
 from .health import router as health_router
@@ -44,6 +44,7 @@ __all__ = [
     "reports_router",
     "discord_router",
     "cgt_governor_router",
+    "evaluation_runtime_router",
     "execution_observability_router",
     "settings_router",
     "applications_router",
