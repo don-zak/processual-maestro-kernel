52679f3410a2de112da069d4e5aa21d15e722b43
zaxam-tech <zaksam2030@gmail.com>
2026-08-31 18:16:17 +0100

    Register evaluation binding catalog routes

--- processual_api/routers/__init__.py
+++ processual_api/routers/__init__.py
@@ -9,6 +9,9 @@ from . import client_api_keys_18 as _client_api_keys_18  # noqa: F401,E402
 from . import client_provider_alias_18 as _client_provider_alias_18  # noqa: F401,E402
 from . import institution_cases_18 as _institution_cases_18  # noqa: F401,E402
 from . import settings_admin_api_key_provisioning as _settings_admin_api_key_provisioning  # noqa: F401,E402
+from . import (
+    settings_admin_evaluation_binding_catalog as _settings_admin_evaluation_binding_catalog,
+)  # noqa: F401,E402
 from . import settings_admin_evaluation_grants as _settings_admin_evaluation_grants  # noqa: F401,E402
 from . import (  # noqa: F401,E402
     settings_enterprise_endpoint_bindings_runtime as _settings_enterprise_endpoint_bindings_runtime,
