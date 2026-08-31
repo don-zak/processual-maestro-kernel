bfbbd68af6e8a0d79e80da6b4f06482d5eb44a89
zaxam-tech <zaksam2030@gmail.com>
2026-08-31 06:21:33 +0100

    Activate CGT external evaluation route guards

--- processual_api/routers/__init__.py
+++ processual_api/routers/__init__.py
@@ -24,6 +24,7 @@ from . import settings_subscription_runtime as _settings_subscription_runtime  #
 from .applications import router as applications_router
 from .cgt import router as cgt_router
 from .cgt_governor import router as cgt_governor_router
+from . import cgt_governor_external_guard as _cgt_governor_external_guard  # noqa: F401,E402
 from .discord import router as discord_router
 from .execution_observability import router as execution_observability_router
 from .governance import router as governance_router
